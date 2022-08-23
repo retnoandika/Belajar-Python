85b6f6a5663f4e0b43aef931f1e08240ccb96ec9
retnoandika <andikaretno154@gmail.com>
2022-08-23 11:09:39 +0700

    commit

--- 16 Date and Time.py
+++ 16 Date and Time.py
@@ -1,1 +1,20 @@
-print(f'Retno Andika = {5 * 34}')
+# Date and Time
+
+import datetime as dt
+
+hari_ini = dt.date.today()
+print(hari_ini)
+print(f"Hari ini adalah hari = {hari_ini:%A}") # %A = hari
+
+tanggal = dt.date(2001,4,15)
+print(tanggal)
+print(f"Hari ini adalah hari = {tanggal:%A}")
+
+
+
+
+
+
+
+
+
